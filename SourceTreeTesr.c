4c4bd983f082b004ef137ec3f9281d28c5df434c
zh5r7hgkqb-ui <rlaals050125@naver.com>
2025-11-03 14:24:07 +0900

    Merge branch 'test'
    # Conflicts:
    #	SourceTreeTesr.c

--- SourceTreeTesr.c
+++ SourceTreeTesr.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main(){
+
+    int i=0; 
+
     printf("hello world\n");
     printf("my name is hooyoien\n");
     printf("메인 브렌치에 추가했음");
+    for(i=0; i<5; i++){
+        printf("테스트 브렌치\n", i);
+    }
+
+    printf("끝\n");
     return 0;
 }
